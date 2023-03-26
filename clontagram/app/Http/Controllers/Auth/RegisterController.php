17b7f994e10744fef9e7dfb1f10cc15df4ffe931
gabmart1995 <gabmart1995@gmail.com>
2023-03-26 12:14:59 -0400

    wip: user register done

--- clontagram/app/Http/Controllers/Auth/RegisterController.php
+++ clontagram/app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -51,6 +51,8 @@ class RegisterController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
+            'surname' => 'required|string|max:255',
+            'nick' => 'required|string|max:255',
         ]);
     }
 
@@ -66,6 +68,9 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
+            'surname' => $data['surname'],
+            'nick' => $data['nick'],
+            'role' => 'user',
         ]);
     }
 }
